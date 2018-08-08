2b5108b884f84450356b8fa5502d5501f9e8b99f
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-08-08 16:02:15 -0600

    Update datosempleados.php

--- application/views/datosempleados.php
+++ application/views/datosempleados.php
@@ -11,7 +11,12 @@
 	<label for="apellidosusuario">Apellidos:</label>
 		<input type="text" name="apellidos" placeholder="Digite apellidos"><br><br>
 	<label for="direccion">Direccion</label>
-		<input type="text" name="direccion"><br><br>
+		<input type="text" name="direccion" placeholder="Digite direccion"><br><br>
+	<label for="Cargo">Cargo</label>
+		<input type="text" name="cargo" Placeholder="Digite Cargo"><br><br>
+	<label for="telefonoempleado">Telefono empleado</label>
+		<input type="text" name="telefonoempleado" placeholder="Digite telefono"><br><br>
+		
 		<input type="submit" name="ingresar" value="Ingresar">
 </body>
 </html>
