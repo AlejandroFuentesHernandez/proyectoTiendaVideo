80abc0dbb399ac763a69e4f1b9cf01145b52b00c
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-08-08 15:47:53 -0600

    Update datosvendedores.php

--- application/views/datosvendedores.php
+++ application/views/datosvendedores.php
@@ -6,8 +6,21 @@
 </head>
 <body>
 	<form action="" method="POST">
-	<label for="Nombreusuario">Nombre</label>
-		<input type
+	<label for="nombreusuario">Nombre:</label>
+		<input type="text" name="nombreusuario" placeholder="Digite nombre"><br><br>
+	<label for="apellidosusuario">Apellidos:</label>
+		<input type="text" name="apellidos" placeholder="Digite apellidos"><br><br>
+	<label for="direccion">Direccion</label>
+		<input type="text" name=""
+		
+		
+		
+		
+		
+		
+		
+		
+		<input type="submit" name="ingresar" value="Ingresar">
 </body>
 </html>
 ?>
