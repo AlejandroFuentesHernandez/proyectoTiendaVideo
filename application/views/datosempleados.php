f9234dab1f444824a1d401aaca5bafdc36324396
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-08-08 15:51:28 -0600

    Add files via upload

---
+++ application/views/datosempleados.php
@@ -0,0 +1,26 @@
+<?php
+<html>
+<head>
+<meta charset='utf-8'>
+<title>Ingresar nuevos usuarios</title>
+</head>
+<body>
+	<form action="" method="POST">
+	<label for="nombreusuario">Nombre:</label>
+		<input type="text" name="nombreusuario" placeholder="Digite nombre"><br><br>
+	<label for="apellidosusuario">Apellidos:</label>
+		<input type="text" name="apellidos" placeholder="Digite apellidos"><br><br>
+	<label for="direccion">Direccion</label>
+		<input type="text" name=""
+		
+		
+		
+		
+		
+		
+		
+		
+		<input type="submit" name="ingresar" value="Ingresar">
+</body>
+</html>
+?>
