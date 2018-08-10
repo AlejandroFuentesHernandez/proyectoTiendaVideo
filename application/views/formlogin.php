e8102ac52fd5029805f19033f357116966c03585
nano13 <41400371+nano1310@users.noreply.github.com>
2018-08-10 18:47:31 +0200

    vichos
    aqui esta el mf login

---
+++ application/views/formlogin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/css/style.css">
+</head>
+<body style="padding: 40px">
+<form action="<?php echo site_url(); ?>/Login/iniciosesion" method=POST>
+	<font color="#0184ff"><h1>Inicio de sesion de usuarios </h1></font>
+<div class="inset">
+       <label>Ingrese su usuario<input type="text" name="usuario" placeholder="usuario" autocomplete="off"></label>
+	   <label>Digite su contraseña<input type="password" name="password" placeholder="contraseña" ></label>
+	   <input type="submit" name="login"  value="iniciar sesion">
+</div>
+</form>
